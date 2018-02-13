ea276d90e3cc17fd7261dfa5ee5f1c345886184d
QilongHai <allenhai@protonmai.com>
2018-02-14 02:30:42 +0800

    add some dir

---
+++ data-structures/array/dynamic_array.py
@@ -0,0 +1,49 @@
+import ctypes
+
+class DynamicArray:
+    def __init__(self):
+        self._n = 0  # to count the elements
+        self._capacity = 1  # default array capacity
+        self._A = self._make_array(self._capacity)
+
+    def __len__(self):
+        return self._n
+
+    def __getitem__(self, k):
+        """Return element at index k."""
+        if not 0 <= k < self._n:
+            raise IndexError('invalid index')
+        return self._A[k]
+
+    def append(self, object):
+        if self._n == self._capacity:  # no enough room to store the object
+            self._resize(2 * self._capacity)
+        self._A[self._n] = object
+        self._n += 1
+
+    def _resize(self, c):
+        """Resize internal array to capacity c."""
+        B = self._make_array(c)
+        for i in range(self._n):
+            B[i] = self._A[i]
+        self._A = B
+        self._capacity = c
+
+    def _make_array(self, c):
+        """Return new array with capacity c."""
+        return (c * ctypes.py_object)()
+
+    def insert(self, k, value):
+        pass
+
+    def remove(self, value):
+        pass
+
+if __name__ == '__main__':
+    A = DynamicArray()
+    A.append(12)
+    A.append(13)
+    A.append(14)
+    for i in A:
+        print(i)
+    print(len(A))
