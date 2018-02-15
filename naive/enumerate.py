452588cb28c7ac37d7df8a80d53112e029d110bb
QilongHai <allenhai@protonmai.com>
2018-02-15 19:44:36 +0800

    implementing enumerate with yield

---
+++ naive/enumerate.py
@@ -0,0 +1,12 @@
+def enumerate(data):
+    '''Generates an indexed series:  (0,coll[0]), (1,coll[1]) ...'''
+    i = 0
+    it = iter(data)
+    while 1:
+        yield (i, it.__next__())
+        i += 1
+
+if __name__ == '__main__':
+    A = ['allen', 'lucas', 'bob', 'finch', 'susan']
+    for i, j in enumerate(A):
+        print(i, j)
