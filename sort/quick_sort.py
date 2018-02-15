37534d895ac88395ea4bebd569b2688a10d39b11
QilongHai <allenhai@protonmai.com>
2018-02-15 20:42:47 +0800

    add quick sort

---
+++ sort/quick_sort.py
@@ -0,0 +1,8 @@
+def quick_sort(A):
+    if len(A) <= 1:
+        return A
+    else:
+        p = A[0]
+        small_part = [x for x in A[1:] if x < p]
+        large_part = [x for x in A[1:] if x >= p]
+        return small_part + [p] + large_part
