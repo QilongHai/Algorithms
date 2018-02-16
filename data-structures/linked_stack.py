cf4a4bdf4a979b0a8ba263c5bfd02122142f4f5d
QilongHai <allenhai@protonmai.com>
2018-02-16 23:51:51 +0800

    Stack implementation using a singly linked list.

---
+++ data-structures/linked_stack.py
@@ -0,0 +1,36 @@
+class Empty(Exception):
+    pass
+
+class LinkedStack:
+    """Stack implementation using a singly linked list."""
+    class _Node:
+        __slots__ = '_elemment', '_next'
+        def __init__(self, element, next):
+            self._elemment = element
+            self._next = next
+    def __init__(self):
+        self._head = None
+        self._size = 0
+
+    def __len__(self):
+        return self._size
+
+    def is_empty(self):
+        return self._size == 0
+
+    def top(self):
+        if self.is_empty():
+            raise Empty('stack is empty')
+        return self._head._element
+
+    def push(self, e):
+        self._head = self._Node(e, self._head)
+        self._size += 1
+
+    def pop(self):
+        if self.is_empty():
+            raise Empty('stack is empty')
+        answer = self._head._element
+        self._head = self._head._next
+        self._size -= 1
+        return answer
