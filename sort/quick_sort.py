31e03af8880941e9b895402c10a6b5c91b2b8d8e
QilongHai <allenhai@protonmai.com>
2018-02-24 01:56:11 +0800

    add some comment about how to choice the pivot

--- sort/quick_sort.py
+++ sort/quick_sort.py
@@ -2,7 +2,8 @@ def quick_sort(A):
     if len(A) <= 1:
         return A
     else:
-        p = A[0]
+        p = A[0]  # another method: from random import choice and set p = choice(A)
         small_part = [x for x in A[1:] if x < p]
         large_part = [x for x in A[1:] if x >= p]
         return small_part + [p] + large_part
+
