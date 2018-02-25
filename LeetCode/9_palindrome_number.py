b2c2bb4c91e9ae38f15da79c44c0eea3a6bf7b72
QilongHai <allenhai@protonmai.com>
2018-02-25 16:59:53 +0800

    add palindrome number

--- LeetCode/9_palindrome_number.py
+++ LeetCode/9_palindrome_number.py
@@ -2,12 +2,10 @@ def palindrome_number(x):
     if x < 0:
         return False
     copy, reverse = x, 0
-
     while copy:
         reverse *= 10
         reverse += copy % 10
         copy //= 10
-
     return x == reverse
 
 if __name__ == '__main__':
