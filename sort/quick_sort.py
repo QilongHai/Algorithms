c01853f27cc1a5a102e91da0e6e67e017f62b584
lucashood <AllenHai@protonmail.com>
2018-04-01 00:03:26 +0800

    Update quick_sort.py

--- sort/quick_sort.py
+++ sort/quick_sort.py
@@ -5,5 +5,5 @@ def quick_sort(A):
         p = A[0]  # another method: from random import choice and set p = choice(A)
         small_part = [x for x in A[1:] if x < p]
         large_part = [x for x in A[1:] if x >= p]
-        return small_part + [p] + large_part
+        return quick_sort(small_part) + [p] + quick_sort(large_part)
 
