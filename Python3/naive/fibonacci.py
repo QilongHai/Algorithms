a65f7ed05a19aaa3671455bddd454735756e0f5a
qlhai <qilonghai1994@gmail.com>
2019-05-02 23:32:07 +0800

    Format code

--- Python3/naive/fibonacci.py
+++ Python3/naive/fibonacci.py
@@ -1,13 +1,18 @@
 def f1(n):
-    if n < 2: return 1
-    else: return f1(n-1) + f1(n-2)
+    if n < 2: 
+        return 1
+    else: 
+        return f1(n-1) + f1(n-2)
+
 
 def f2(n):
-    if n < 2: return 1
+    if n < 2: 
+        return 1
     memo = [1, 1]
     for i in range(2, n+1):
         memo.append(memo[i-1] + memo[i-2])
     return memo[n]
+
 
 def f3(n):
     memo = {}
@@ -20,6 +25,7 @@ def f3(n):
             f = f3(n-1) + f3(n-2)
         memo[n] = f
         return f
+
 
 if __name__ == '__main__':
     n = 23
