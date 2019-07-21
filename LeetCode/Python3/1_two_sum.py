ca610efe836443340000fd1e838718017e6a95c0
qlhai <qilonghai1994@gmail.com>
2019-07-21 10:30:57 +0800

    Modify： two_sum

--- LeetCode/Python3/1_two_sum.py
+++ LeetCode/Python3/1_two_sum.py
@@ -1,17 +1,18 @@
 def two_sum_naive(nums, target):
-    for i in range(nums):
+    for i in range(len(nums)):
         for j in range(i+1, len(nums)):
             if nums[i] + nums[j] == target:
                 return [i, j]
 
 def two_sum_hash(nums, target):
-    d = {}
+    d = dict()
     for index, value in enumerate(nums):
-        k = target - value
-        if k in d:
-            return [d[k], index]
+        tmp = target - value
+        if tmp in d:
+            return [index, d[tmp]]
         else:
             d[value] = index
+
 
 if __name__ == '__main__':
     nums = [1, 2, 4, 5, 9, 13]
