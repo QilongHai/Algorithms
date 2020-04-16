1be91531206d1ce95b6b20a50b1394f3ee1c4e71
Qilong Hai <qlhai@outlook.com>
2020-04-16 15:27:43 +0800

    Update 00016.py

--- LeetCode/Python3/00016.py
+++ LeetCode/Python3/00016.py
@@ -1,35 +1,30 @@
-from typing import List
-
-
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        length = len(nums)
-        if length == 3:
+        n = len(nums)
+        if n == 3:
             return sum(nums)
-
-        min_diff = float("inf")
-        closest_sum = 0
         nums.sort()
-
-        for i in range(length - 2):
-            if i > 1 and nums[i] == nums[i - 1]:
+        min_diff = float('inf')
+        closet_sum = 0
+        for i in range(n-2):
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
-            left = i + 1
-            right = length - 1
-            while left < right:
-                three_sum = nums[i] + nums[left] + nums[right]
-                diff = three_sum - target
+            low = i + 1
+            high = n - 1
+            while low < high:
+                tmp = nums[i] + nums[low] + nums[high]
+                diff = tmp - target
                 if abs(diff) < min_diff:
                     min_diff = abs(diff)
-                    closest_sum = three_sum
+                    closet_sum = tmp
                 if diff == 0:
-                    return closest_sum
+                    return closet_sum
                 elif diff < 0:
-                    left += 1
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
+                    low += 1
+                    while low < high and nums[low] == nums[low-1]:
+                        low += 1
                 else:
-                    right -= 1
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
-        return closest_sum
+                    high -= 1
+                    while low < high and nums[high] == nums[high+1]:
+                        high -= 1
+        return closet_sum
