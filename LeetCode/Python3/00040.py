0e0e5c5fb022e656e80b20c433c34b475d5f1af3
Qilong Hai <qlhai@outlook.com>
2020-04-20 11:17:20 +0800

    Update and rename 40.py to 00040.py

--- LeetCode/Python3/00040.py
+++ LeetCode/Python3/00040.py
@@ -2,32 +2,31 @@ from typing import List
 
 
 class Solution:
-
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-
-        def dfs(start, path, target):
+        def dfs(candidates, begin, size, path, res, target):
             if target == 0:
                 res.append(path[:])
                 return
-
-            for idx in range(start, size):
-                if candidates[idx] > target:
+            for idx in range(begin, size):
+                cur_val = target - candidates[idx]
+                if cur_val < 0:
                     break
-
-                if idx > start and candidates[idx - 1] == candidates[idx]:
+                if idx > begin and candidates[idx] == candidates[idx-1]:
                     continue
-
                 path.append(candidates[idx])
-                dfs(idx + 1, path, target - candidates[idx])
+                dfs(candidates, idx+1, size, path, res, cur_val)
                 path.pop()
-
+            
+        
         size = len(candidates)
         if size == 0:
             return []
-
-        candidates.sort()
         res = []
-        dfs(0, [], target)
+        path = []
+        begin = 0
+        candidates.sort()
+        dfs(candidates, begin, size, path, res, target)
+
         return res
 
 # 作者：liweiwei1419
