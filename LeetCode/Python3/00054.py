f9599a22af5a1188f82d3a5aa7a3b7062dd70cb3
Qilong Hai <qlhai@outlook.com>
2020-04-20 16:14:58 +0800

    Create 00054.py

---
+++ LeetCode/Python3/00054.py
@@ -0,0 +1,18 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        if not matrix:
+            return []
+        res = []
+        while matrix:
+            res += matrix.pop(0)
+            if matrix and matrix[0]:
+                for row in matrix:
+                    res.append(row.pop())
+            if matrix and matrix[0]:
+                for item in matrix.pop()[::-1]:
+                    res.append(item)
+            if matrix and matrix[0]:
+                for row in matrix[::-1]:
+                    res.append(row.pop(0))
+        
+        return res
