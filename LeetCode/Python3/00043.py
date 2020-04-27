e7d09c80adcc7ccef6959d3b8919bbfc9a0b8529
“j-haiqilong-jk” <haiqilong-jk@360jinrong.net>
2020-04-27 14:51:13 +0800

    update solution of 43

--- LeetCode/Python3/00043.py
+++ LeetCode/Python3/00043.py
@@ -19,3 +19,22 @@ class Solution:
             pt += 1
 
         return ''.join(map(str, product[pt:]))
+
+
+class SolutionTwo:
+    def multiply(self, num1: str, num2: str) -> str:
+        """ref: https://www.cnblogs.com/grandyang/p/4395356.html"""
+        if num1 == '0' or num2 == '0':
+            return '0'
+        m = len(num1)
+        n = len(num2)
+        res = [0] * (m + n)
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                mul = int(num1[i]) * int(num2[j])
+                p1 = i + j
+                p2 = i + j + 1
+                tmp = mul + res[p2]
+                res[p1] += tmp // 10
+                res[p2] = tmp % 10
+        return ''.join(res).lstrip('0')
