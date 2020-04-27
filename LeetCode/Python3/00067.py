4ba3c75f9b5f23db403bcbc799255237d2317221
“j-haiqilong-jk” <haiqilong-jk@360jinrong.net>
2020-04-27 14:20:52 +0800

    update solution of 67

--- LeetCode/Python3/00067.py
+++ LeetCode/Python3/00067.py
@@ -1,17 +1,20 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        carry = 0
+        if not a and not b:
+            return ''
         a = a.strip()
         b = b.strip()
+        carry = 0
         i = len(a) - 1
         j = len(b) - 1
-        stack = []
+        res = []
         while carry or i >= 0 or j >= 0:
             tmp = carry + (int(a[i]) if i >= 0 else 0) + (int(b[j]) if j >= 0 else 0)
-            cur_val = tmp % 2
             carry = tmp // 2
-            stack.append(str(cur_val))
+            cur_val = tmp % 2
+            res.append(str(cur_val))
             i -= 1
             j -= 1
-
-        return ''.join(stack[::-1])
+        if carry > 0:
+            res.append(str(carry))
+        return ''.join(res[::-1])
