540a5108420137663aeec6f8ddfecf6eea7883bc
qlhai <qlhai@outlook.com>
2020-05-03 17:01:18 +0800

    Add solution of 120, 121

---
+++ LeetCode/Python3/00000-00500/00120.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        if not triangle:
+            return 0
+        n = len(triangle)
+        for row in range(n - 2, -1, -1):
+            for col in range(len(triangle[row])):
+                triangle[row][col] += min(triangle[row + 1][col], triangle[row + 1][col + 1])
+        return triangle[0][0]
