3f4dac1203926586e3ee68a1cbb8b2f5112ccf96
qlhai <qlhai@outlook.com>
2020-05-07 12:18:16 +0800

    Update solution of 2

--- LeetCode/Python3/00000-00500/00002.py
+++ LeetCode/Python3/00000-00500/00002.py
@@ -6,14 +6,15 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        pre = head = ListNode(-1)
+        pre = ListNode(-1)
+        cur = pre
         carry = 0
         while l1 or l2 or carry:
             tmp = carry + (l1.val if l1 else 0) + (l2.val if l2 else 0)
             carry = tmp // 10
             cur_val = tmp % 10
-            head.next = ListNode(cur_val)
-            head = head.next
+            cur.next = ListNode(cur_val)
+            cur = cur.next
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
         return pre.next
