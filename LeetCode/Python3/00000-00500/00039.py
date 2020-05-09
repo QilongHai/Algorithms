197f8b49b9bf75a46f277978ca29f4370cf382a3
qlhai <qlhai@outlook.com>
2020-05-09 20:35:35 +0800

    Update solution 39

--- LeetCode/Python3/00000-00500/00039.py
+++ LeetCode/Python3/00000-00500/00039.py
@@ -3,24 +3,63 @@ from typing import List
 
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        def dfs(candidates, begin, size, res, path, target):
-            if target == 0:
+        def dfs(start_idx, path, cur_target):
+            if cur_target == 0:
                 res.append(path[:])
                 return
-            for index in range(begin, size):
-                cur_val = target - candidates[index]
+            for j in range(start_idx, size):
+                cur_val = cur_target - candidates[j]
                 if cur_val < 0:
                     break
-                path.append(candidates[index])
-                dfs(candidates, index, size, res, path, cur_val)
+                path.append(candidates[j])
+                dfs(j, path, cur_val)
                 path.pop()
 
         size = len(candidates)
         if size == 0:
             return []
         candidates.sort()
-        path = []
         res = []
-        dfs(candidates, 0, size, res, path, target)
+        dfs(0, [], target)
 
+        return res
+
+
+class SolutionTwo:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        n = len(candidates)
+        res = []
+
+        def dfs(idx, cur_sum, path):
+            if cur_sum > target or idx == n:
+                return
+            if cur_sum == target:
+                res.append(path[:])
+                return
+            dfs(idx, cur_sum + candidates[idx], path + [candidates[idx]])
+            dfs(idx + 1, cur_sum, path)
+
+        dfs(0, 0, [])
+        return res
+
+
+class SolutionThree:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        n = len(candidates)
+        res = []
+
+        def dfs(idx, cur_sum, path):
+            if idx >= n or cur_sum > target:
+                return
+            if cur_sum == target:
+                res.append(path[:])
+                return
+            for j in range(idx, n):
+                if candidates[j] + cur_sum > target:
+                    break
+                dfs(j, candidates[j] + cur_sum, [candidates[j]] + path)
+
+        dfs(0, 0, [])
         return res
