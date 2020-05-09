aee1d90c2032bb48d23be149755264e71c10ba0e
qlhai <qlhai@outlook.com>
2020-05-10 03:16:54 +0800

    Update solution of 40

--- LeetCode/Python3/00000-00500/00040.py
+++ LeetCode/Python3/00000-00500/00040.py
@@ -28,5 +28,53 @@ class Solution:
 
         return res
 
+
 # 作者：liweiwei1419
 # 链接：https://leetcode-cn.com/problems/combination-sum-ii/solution/hui-su-suan-fa-jian-zhi-python-dai-ma-java-dai-m-3/
+
+class SolutionTwo:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        n = len(candidates)
+        candidates.sort()
+        res = []
+
+        def dfs(idx, cur_sum, path):
+            if cur_sum == 0:
+                res.append(path[:])
+                return
+            for j in range(idx, n):
+                if j > idx and candidates[j] == candidates[j - 1]:
+                    continue
+                tmp = cur_sum - candidates[j]
+                if tmp < 0:
+                    break
+                path.append(candidates[j])
+                dfs(j + 1, tmp, path)
+                path.pop()
+
+        dfs(0, target, [])
+        return res
+
+
+class SolutionThree:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        n = len(candidates)
+        candidates.sort()
+        res = []
+
+        def dfs(idx, cur_sum, path):
+            if cur_sum == target:
+                res.append(path[:])
+                return
+            for j in range(idx, n):
+                tmp = cur_sum + candidates[j]
+                if tmp > target:
+                    break
+                if j > idx and candidates[j] == candidates[j - 1]:
+                    continue
+                path.append(candidates[j])
+                dfs(j + 1, tmp, path)
+                path.pop()
+
+        dfs(0, 0, [])
+        return res
