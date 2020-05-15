5d8403b0b53c18ce1f6c25b1e7d1309b6cdde094
qlhai <qlhai@outlook.com>
2020-05-16 00:04:11 +0800

    Update

--- LeetCode/Python3/00000-00500/00338.py
+++ LeetCode/Python3/00000-00500/00338.py
@@ -4,9 +4,9 @@ from typing import List
 class Solution:
     def countBits(self, num: int) -> List[int]:
         dp = [0] * (num + 1)
-        for i in range(1, num+1):
+        for i in range(1, num + 1):
             if i % 2:
-                dp[i] = dp[i-1] + 1
+                dp[i] = dp[i - 1] + 1
             else:
                 dp[i] = dp[i // 2]
         return dp
