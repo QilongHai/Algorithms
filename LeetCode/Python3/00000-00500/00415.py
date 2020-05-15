7cd9dc662a333aa987e0ca537517f923a6269c4f
qlhai <qlhai@outlook.com>
2020-05-15 13:57:21 +0800

    Update solution of 415

--- LeetCode/Python3/00000-00500/00415.py
+++ LeetCode/Python3/00000-00500/00415.py
@@ -1,16 +1,18 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
+        if not num1 or not num2:
+            return ''
         i = len(num1) - 1
         j = len(num2) - 1
+        k = max(len(num1), len(num2)) + 1
         carry = 0
-        res = []
+        res = ['' for _ in range(k)]
+        k -= 1
         while carry or i >= 0 or j >= 0:
             tmp = carry + (int(num1[i]) if i >= 0 else 0) + (int(num2[j]) if j >= 0 else 0)
             carry = tmp // 10
-            cur_val = tmp % 10
-            res.append(str(cur_val))
+            res[k] = str(tmp % 10)
             i -= 1
             j -= 1
-        if carry > 0:
-            res.append(str(carry))
-        return ''.join(res[::-1])
+            k -= 1
+        return ''.join(res).strip()
