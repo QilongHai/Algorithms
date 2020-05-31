a560466487f70e37df47c38d209318bd42a767dc
qlhai <qlhai@outlook.com>
2020-06-01 00:10:43 +0800

    Update

--- 剑指offer/014-1.py
+++ 剑指offer/014-1.py
@@ -1,8 +1,8 @@
 class Solution:
     def cuttingRopeRecursion(self, n: int) -> int:
         """
-        设 F(n) 为长度为 n 的绳子可以得到的最大乘积
-        F(n) = max(i*(n−i), i*F(n−i)), i=1, 2, ..., n−2
+        设 f(n) 为长度为 n 的绳子可以得到的最大乘积
+        f(n) = max(1 * f(n - 1), 2 * f(n - 2), ..., (n - 1) * f(1))
         """
         if n == 2:
             return 1
@@ -17,8 +17,6 @@ class Solution:
         dp = [0 for _ in range(n+1)]
         dp[2] = 1
         for i in range(3, n+1):
-            for j in range(i):
+            for j in range(1, i):
                 dp[i] = max(dp[i], max((i - j) * j, j * dp[i-j]))
         return dp[n]
-
-        
