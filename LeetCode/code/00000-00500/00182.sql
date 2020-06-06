db17fa2752009b7ad20b6431e3b77b522efa552e
qlhai <qlhai@outlook.com>
2020-06-06 22:40:35 +0800

    Format 182, 197

--- LeetCode/code/00000-00500/00182.sql
+++ LeetCode/code/00000-00500/00182.sql
@@ -1,15 +1,15 @@
 select Email
 from (select Email, count(Email) as cnt
-     from Person
-     group by Email) as statistic
- where cnt > 1
+    from Person
+    group by Email) as statistic
+where cnt > 1
 
- select Email
- from Person
- group by Email
- having count(Email) > 1
+select Email
+from Person
+group by Email
+having count(Email) > 1
 
- select distinct a.Email as 'Email'
- from Person as a
- join Person as b
- on a.Email = b.Email and a.Id != b.Id
+select distinct a.Email as 'Email'
+from Person as a
+    join Person as b
+    on a.Email = b.Email and a.Id != b.Id
--- LeetCode/code/00000-00500/00197.sql
+++ LeetCode/code/00000-00500/00197.sql
@@ -2,4 +2,4 @@ select a.Id as 'Id'
 from Weather as a
     join Weather as b
     on datediff(a.RecordDate, b.RecordDate) = 1
-    where a.Temperature > b.Temperature
+where a.Temperature > b.Temperature
