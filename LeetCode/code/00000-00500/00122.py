02db80f1304b50e46e3b781c792fc9f5acc1aa30
qlhai <qlhai@outlook.com>
2020-06-07 13:08:08 +0800

    Update solution of 122

--- LeetCode/code/00000-00500/00122.py
+++ LeetCode/code/00000-00500/00122.py
@@ -9,3 +9,30 @@ class Solution:
             if tmp > 0:
                 profit_sum += tmp
         return profit_sum
+
+
+class SolutionTwo:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
+        n = len(prices)
+        dp = [[0, 0] for _ in range(n)]
+        dp[0][0] = 0
+        dp[0][1] = -prices[0]
+        for i in range(1, n):
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0]-prices[i])
+        return dp[n-1][0]
+
+
+class SolutionThree:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
+        dp_0 = 0
+        dp_1 = -prices[0]
+        for i in range(1, len(prices)):
+            yesterday_dp_0 = dp_0
+            dp_0 = max(dp_0, dp_1 + prices[i])
+            dp_1 = max(dp_1, yesterday_dp_0 - prices[i])
+        return dp_0
