6f1b355754e2a8f0e5508e1ee774579321051cf7
qlhai <qlhai@outlook.com>
2020-06-10 13:12:35 +0800

    Add solution of 175

--- LeetCode/sql/00197.sql
+++ LeetCode/sql/00197.sql
@@ -1,5 +1,4 @@
 select a.Id as 'Id'
 from Weather as a
-    join Weather as b
-    on datediff(a.RecordDate, b.RecordDate) = 1
+    join Weather as b on datediff(a.RecordDate, b.RecordDate) = 1
 where a.Temperature > b.Temperature
