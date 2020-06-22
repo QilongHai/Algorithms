9e8a041007968cab11dbb605f118d6ffcf3f865d
qlhai <qilonghai1994@gmail.com>
2020-06-22 11:22:27 +0800

    Add solution of lcof 54, 68

--- 剑指offer/026.py
+++ 剑指offer/026.py
@@ -12,3 +12,25 @@ class Solution:
         if not A or not B:
             return False
         return helper(A, B) or self.isSubStructure(A.left, B) or self.isSubStructure(A.right, B)
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class SolutionTwo:
+    def isSubStructure(self, A: TreeNode, B: TreeNode) -> bool:
+        if not A or not B:
+            return False
+        return self.dfs(A, B) or self.isSubStructure(A.left, B) or self.isSubStructure(A.right, B)
+
+    def dfs(self, a, b):
+        if not b:
+            return True
+        if not a:
+            return False
+        return a.val == b.val and self.dfs(a.left, b.left) and self.dfs(a.right, b.right)
