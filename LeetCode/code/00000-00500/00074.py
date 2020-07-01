81b34140150e8f00a4efd32ced06baaa3e3905af
qlhai <qilonghai1994@gmail.com>
2020-07-02 00:34:05 +0800

    Update solution of 74, 304

--- LeetCode/code/00000-00500/00074.py
+++ LeetCode/code/00000-00500/00074.py
@@ -3,18 +3,40 @@ from typing import List
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        if not matrix:
+        if not matrix or not matrix[0]:
             return False
-        row = len(matrix) - 1
-        col = len(matrix[0]) - 1
-        i = row
-        j = 0
-        while i >= 0 and j <= col:
-            if matrix[i][j] == target:
+        m = len(matrix)
+        n = len(matrix[0])
+        start_row = 0
+        end_row = m - 1
+        start_col = n - 1
+        end_col = 0
+        while start_row <= end_row and start_col >= end_col:
+            if matrix[start_row][start_col] == target:
                 return True
-            elif matrix[i][j] < target:
-                j += 1
+            elif matrix[start_row][start_col] > target:
+                start_col -= 1
             else:
-                i -= 1
+                start_row += 1
+        return False
 
+
+class SolutionTwo:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix or not matrix[0]:
+            return False
+        m = len(matrix)
+        n = len(matrix[0])
+        low = 0
+        high = m * n - 1
+        while low <= high:
+            mid = low + (high - low) // 2
+            row = mid // n
+            col = mid % n
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] > target:
+                high = mid - 1
+            else:
+                low = mid + 1
         return False
--- LeetCode/code/00000-00500/00304.py
+++ LeetCode/code/00000-00500/00304.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-class NumMatrix:
+class c:
 
     def __init__(self, matrix: List[List[int]]):
         self.matrix = matrix
@@ -17,3 +17,21 @@ class NumMatrix:
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
 # param_1 = obj.sumRegion(row1,col1,row2,col2)
+
+
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        self.matrix = matrix
+        if not matrix not matrix[0]:
+            return
+        m = len(matrix)
+        n = len(matrix[0])
+        self.dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                self.dp[i][j] = matrix[i-1][j-1] + self.dp[i-1][j] + \
+                    self.dp[i][j] - self.dp[i-1][j-1]
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        return self.dp[row2+1][col2+1] - self.dp[row1][col2+1] - self.dp[row2+1][col1] + self.dp[row1][col1]
